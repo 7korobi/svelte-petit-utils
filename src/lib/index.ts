7d573997b9856fd74cff93f0671ae11b713f2258
7korobi <7korobi@gmail.com>
2022-09-25 12:45:17 +0900

    init (for sveltekit pr1 )

---
+++ src/lib/index.ts
@@ -0,0 +1,6 @@
+export * from './bits';
+export * from './bits-n';
+export * from './const';
+export * from './mouse';
+export * from './portal';
+export * from './util';
---
+++ src/routes/+page.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { Location } from '$lib';
+
+	let searchParams = {
+		keys: ['a', 'b'], // &keys=a&keys=b
+		key: 'c'
+	};
+	let hash = 'test'; // #test
+	let protocol: string; // http:
+	let host: string; // localhost
+	let port: number; // 3000
+	let hostname: string;
+	let pathname: string;
+	$: console.log({ searchParams, hash, protocol, host, port, hostname, pathname });
+</script>
+
+<Location
+	bind:searchParams
+	bind:hash
+	bind:protocol
+	bind:host
+	bind:port
+	bind:hostname
+	bind:pathname
+/>
+
+<h1>Welcome to your library project</h1>
+<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<p>
+	hash: <input type="text" bind:value={hash} />
+</p>
+<p>
+	searchparams key: <input type="text" bind:value={searchParams.key} />
+</p>
+<p>
+	searchparams keys:
+	{#each searchParams.keys as key, idx (idx)}
+		{idx}: <input type="text" bind:value={key} />
+	{/each}
+	{#each [''] as key, idx (searchParams.keys.length)}
+		(add keys): <input type="text" bind:value={searchParams.keys[searchParams.keys.length]} />
+	{/each}
+</p>
